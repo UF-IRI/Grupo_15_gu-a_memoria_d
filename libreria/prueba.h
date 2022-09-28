726fe7b3a77872e87cbc31536f08813a45a506ad
Thiago Monteros <thmonteros2@gmail.com>
2022-09-27 21:37:39 -0300

    tarea de "hello world, my git" completada

---
+++ libreria/prueba.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace foobar
+{
+    struct Example
+    {
+        int getValue() const;
+    };
+}
+
+float calculadora_simple(double numero_1, double numero_2, int cuenta);
+
+float Bhaskara_dif_positivo(float a, float b, float c);
+float Bhaskara_dif_negativo(float a, float b, float c);
--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,47 @@
 #include <cstdlib>
-#include <iostream>
+#include "iostream"
 
-#include "example.h"
+#include "prueba.h"
+
+using namespace std;
 
 int main(int argc, char** argv)
 {
     foobar::Example example{};
     std::cout << "Example.getValue() => " << example.getValue() << std::endl;
+
+        double numerador = 0;
+        double denominador = 0;
+        int seleccion = 0;
+        bool ciclo = true;
+        double resultado;
+        cout << "ingrese dos numeros, tenga en cuenta el orden en el que son ingresados" << endl;
+        cin >> numerador >> denominador;
+
+        do
+        {
+            cout << "que operacion desea realizar?" << endl << "1.suma" << endl << "2.resta" << endl << "3.multiplicacion" << endl << "4.division" << endl << "5.potenciacion" << endl;
+            cin >> seleccion;
+            if (seleccion > 0 && seleccion < 5)
+            {
+                resultado = calculadora_simple(numerador, denominador, seleccion);
+                ciclo = false;
+            }
+            else
+                cout << "no eligio ninguna cuenta, vuelva a intentarlo";
+        } while (ciclo == true);
+
+        cout << "Su resultado es: " << resultado << endl;
+
+        cout << "para calcular las raices de una ecuacion de segundo grado ingrese los valores de los coeficientes a, b y c, en ese orden" << endl;
+        float a, b, c;
+        a = b = c = 0;
+        cin >> a >> b >> c;
+        float x1, x2;
+        x1 = x2 = 0;
+
+        x1 = Bhaskara_dif_positivo(a, b, c);
+        x2 = Bhaskara_dif_negativo(a, b, c);
 
     return EXIT_SUCCESS;
 }
